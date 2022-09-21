f134cb07192f3357169d0776dd9160cd9b52916f
UNOSQUARE\arturo.jimenez <arturojimenez-unosquare>
2022-09-21 17:12:08 -0500

    Upload Counter Class Project - Initial Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+ 
+class ClassComponent extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            count :0
+        };
+        this.increase=this.increase.bind(this);
+        this.decrease=this.decrease.bind(this);
+    }
+     
+   increase(){
+       this.setState({count : this.state.count +1});
+   }
+
+   decrease(){
+       this.setState({count : this.state.count -1});
+   }
+ 
+    render(){
+        return (
+            <div style={{margin:'50px'}}>
+               <h1>Welcome to Geeks for Geeks </h1>
+               <h3>Counter App using Class Component : </h3>
+               <h2> {this.state.count}</h2> 
+               <button onClick={this.increase}> Increase</button>
+               <button onClick={this.decrease}> Decrease</button>
+ 
+            </div>
+        )
+    }
+}
+ 
+export default ClassComponent;
